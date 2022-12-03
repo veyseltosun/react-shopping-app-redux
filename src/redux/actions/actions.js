bd36359af29c986ffa42a686a8553eb76587267c
veyseltosun <postalarim1923@gmail.com>
2022-12-03 08:03:21 +0300

    action.

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,31 +1,42 @@
-import { SELL_LAPTOP, SELL_COMP, _SELL_WATCH, SELL_PHONE } from "../types/types";
+import { SELL_LAPTOP, SELL_COMP, SELL_WATCH, SELL_PHONE } from "../types/types";
 
 
 export const sellLaptop = () =>{
-    return  
-     type: SELL_LAPTOP,
-        
+    return  {
+
+        type: SELL_LAPTOP,
+    }
 
     
 } ;
-export const sellLaptop = () =>{
-    return  
-     type: SELL_LAPTOP,
-        
+export const sellComp = () =>{
+    return  {
+
+        type: SELL_COMP,
+    }
+
+    ;
+} ;
+export const sellPhone = () =>{
+    return  {
+
+        type: SELL_PHONE,
+    }
 
     
 } ;
-export const sellLaptop = () =>{
-    return  
-     type: SELL_LAPTOP,
-        
+export const sellWatch = () =>{
+    return {
+
+        type: SELL_WATCH,
+    } 
 
     
 } ;
-export const sellLaptop = () =>{
-    return  
-     type: SELL_LAPTOP,
+
+    
+
+    
         
 
     
-} ;
