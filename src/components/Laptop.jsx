f24cf010b11fa45e1cf6787a9130f03b15f40113
veyseltosun <postalarim1923@gmail.com>
2022-12-05 00:54:10 +0300

    there is bug here I cant fix it. connect methods ı dont if its send the props or not.

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {connect} from "react-redux";
 
-function Laptop(props) {
+function Laptop({numberOfLaptops}) {
+    console.log("props");
+    console.log("props.numberOfLaptops", numberOfLaptops);
   return (
     <div>
         <h2>Laptops  --- Pure Redux ---</h2>
-        <h3>Nuber of Laptops:{props.numberOfLaptops}
+        <h3>Nuber of Laptops:{numberOfLaptops}
             
         </h3>
     </div>
   )
 }
 const mapStateToProps = (state) =>{
+    console.log("state:", state)
+    console.log(connect)
     return {
         numberOfLaptops:state.laptop.numberOflaptops
     }
