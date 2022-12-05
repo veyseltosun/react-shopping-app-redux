95c3b90669d36ac394fb012bb2cbc1560dd50709
veyseltosun <postalarim1923@gmail.com>
2022-12-05 08:45:03 +0300

    index js is renamed the root to index ı changed the name.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import  store   from "./redux/reducer/root"
+import  store   from "./redux/reducer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
